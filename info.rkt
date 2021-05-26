15a49b88386a453ef2ed07b694cf0d62a42211d2
Yanying Wang <yanyingwang1@gmail.com>
2021-05-26 13:09:33 +0800

    fix info.rkt

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "amap")
 (define deps '("base" "request"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "scribble-rainbow-delimiters"))
 (define scribblings '(("scribblings/amap.scrbl" ())))
 (define pkg-desc "Gaode Amap web service API")
 (define version "0.1")
